417d2cdc270f7bea6025fbb55c16a592940eae65
EriBloo <19932449+EriBloo@users.noreply.github.com>
2024-10-14 22:13:41 +0000

    add json value modifier

---
+++ src/ValueObjects/Values/JsonModifier.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EriBloo\CacheObjects\ValueObjects\Values;
+
+use EriBloo\CacheObjects\Contracts\CacheValueModifier;
+
+/**
+ * @template TValue
+ *
+ * @implements CacheValueModifier<TValue>
+ */
+final readonly class JsonModifier implements CacheValueModifier
+{
+    /**
+     * @param int<1, max> $depth
+     */
+    public function __construct(
+        private int $loadFlags = 0,
+        private int $saveFlags = 0,
+        private int $depth = 512,
+    ) {}
+
+    public function onLoad(string $value): mixed
+    {
+        return json_decode($value, null, $this->depth, JSON_THROW_ON_ERROR | $this->loadFlags);
+    }
+
+    public function onSave(mixed $value): string
+    {
+        return json_encode($value, JSON_THROW_ON_ERROR | $this->saveFlags, $this->depth);
+    }
+}
